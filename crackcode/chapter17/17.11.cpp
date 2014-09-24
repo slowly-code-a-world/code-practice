6a495e4cb3a89f9b5bd08d4a9d701915f898bdb1
Yan Cui <yc2920@columbia.edu>
2014-09-23 21:30:43 -0400

    updates

--- crackcode/chapter17/17.11.cpp
+++ crackcode/chapter17/17.11.cpp
@@ -3,5 +3,22 @@ Implement a method rand7() given rand5().That is, given a method that
 generates a random number between 0 and 4 (inclusive), write a method that
 generates a random number between 0 and 6 (inclusive).
 */
+int rand7() {
+	while (1) {
+		int num = 5*rand5() + rand5();
+		if (num < 21) 
+			return num % 7;
+	}
+}
 
-
+int rand7_v2() {
+	while (1) {
+		int r1 = 2*rand5();
+		int r2 = rand5();
+		if (r2 != 4) {
+			int rand1 = r2%2;
+			int num = r1 + rand1;
+			if (num < 7) return num;
+		}
+	}
+}
