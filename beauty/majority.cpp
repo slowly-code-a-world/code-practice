543ce9eed38731913ca6752fdd14828c240ecd61
Yan Cui <yc2920@columbia.edu>
2015-01-29 12:56:46 -0500

    enhanced majority

--- beauty/majority.cpp
+++ beauty/majority.cpp
@@ -18,4 +18,31 @@ public:
 		}
 		return candidate;
 	}
+
+	vector<int> getMajor(vector<int>&array) {
+		int ntimes[3] = {0}, candidates[3] = {0};
+		for (int i = 0; i<array.size(); i++) {	
+			bool found = false;
+			for (int j = 0; j<3; j++) {
+				if (ntimes[j] > 0 && candidate[j] == array[i]) {
+					ntimes[j]++;
+					found = true;
+					break;
+				}
+			}
+			if (found) continue;
+			found = false;
+			for (int j = 0; j<3; j++) {
+				if (ntimes[j] == 0) {
+					candidates[j] = array[i];
+					ntimes[j] = 1;
+					found = true;
+					break;
+				}
+			}
+			if (found) continue;
+			for (int j = 0; j< 3; j++) ntimes[j] --;
+		}
+		return vector<int>(candidates, candidates + 3);
+	}
 };
